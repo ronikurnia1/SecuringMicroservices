c49f232b3fa70cc5abc974ffb16726c3457e82c5
Roni Kurniawan <rokurnia@microsoft.com>
2022-01-05 23:39:18 +0700

    first commit

---
+++ 02/end/GloboTicket.Services.Identity/Config.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace GloboTicket.Services.Identity
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+
+        public static IEnumerable<ApiResource> ApiResources =>
+          new ApiResource[]
+          {
+              new ApiResource("globoticket", "GlobotTicket APIs")
+              {
+                  Scopes = { "globoticket.fullaccess" }
+              }
+          };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            { 
+                new ApiScope("globoticket.fullaccess")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {    
+                new Client
+                {
+                    ClientName = "GloboTicket Machine 2 Machine Client",
+                    ClientId = "globoticketm2m",
+                    ClientSecrets = { new Secret("eac7008f-1b35-4325-ac8d-4a71932e6088".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = { "globoticket.fullaccess" }
+                },
+                new Client
+                {
+                    ClientName = "GloboTicket Interactive Client",
+                    ClientId = "globoticketinteractive",
+                    ClientSecrets = { new Secret("ce766e16-df99-411d-8d31-0f5bbc6b8eba".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = { "https://localhost:5000/signin-oidc" },
+                    PostLogoutRedirectUris = { "https://localhost:5000/signout-callback-oidc" },
+                    AllowedScopes = { "openid", "profile", "globoticket.fullaccess" }
+                }
+            };
+    }
+}
---
+++ 06/start/GloboTicket.Client/Services/EventCatalogService.cs
@@ -0,0 +1,55 @@
+﻿using GloboTicket.Web.Extensions;
+using GloboTicket.Web.Models.Api;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authentication;
+
+namespace GloboTicket.Web.Services
+{
+    public class EventCatalogService : IEventCatalogService
+    {
+        private readonly HttpClient client;
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public EventCatalogService(HttpClient client,
+            IHttpContextAccessor httpContextAccessor)
+        {
+            this.client = client;
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+
+        public async Task<IEnumerable<Event>> GetAll()
+        {
+            client.SetBearerToken(await httpContextAccessor.HttpContext.GetTokenAsync("access_token"));
+            var response = await client.GetAsync("api/events");
+            return await response.ReadContentAs<List<Event>>();
+        }
+
+        public async Task<IEnumerable<Event>> GetByCategoryId(Guid categoryid)
+        {
+            client.SetBearerToken(await httpContextAccessor.HttpContext.GetTokenAsync("access_token"));
+            var response = await client.GetAsync($"api/events/?categoryId={categoryid}");
+            return await response.ReadContentAs<List<Event>>();
+        }
+
+        public async Task<Event> GetEvent(Guid id)
+        {
+            client.SetBearerToken(await httpContextAccessor.HttpContext.GetTokenAsync("access_token"));
+            var response = await client.GetAsync($"api/events/{id}");
+            return await response.ReadContentAs<Event>();
+        }
+
+        public async Task<IEnumerable<Category>> GetCategories()
+        {
+            client.SetBearerToken(await httpContextAccessor.HttpContext.GetTokenAsync("access_token"));
+            var response = await client.GetAsync("api/categories");
+            return await response.ReadContentAs<List<Category>>();
+        }
+
+    }
+}
